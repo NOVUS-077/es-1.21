d3c3dd084e3fc5a1dcff229c1bfca9ed949ab912
Lucio <lucioborelli94@gmail.com>
2026-05-10 16:18:27 +0200

    Aggiungere i file di progetto.

---
+++ es 1.21/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static Queue<int> numeri = new Queue<int>();
+    static object lockCoda = new object();
+    static bool fine = false;
+
+    static void Main()
+    {
+        Thread produttore = new Thread(Produttore);
+        Thread consumatore = new Thread(Consumatore);
+
+        produttore.Start();
+        consumatore.Start();
+
+        produttore.Join();
+        consumatore.Join();
+
+        Console.WriteLine("Programma terminato.");
+    }
+
+    static void Produttore()
+    {
+        Random rnd = new Random();
+        while (true)
+        {
+            int numero = rnd.Next(0, 11);
+            lock (lockCoda)
+            {
+                numeri.Enqueue(numero);
+                Console.WriteLine($"Prodotto: {numero}");
+                Monitor.Pulse(lockCoda);
+                if (numero == 0)
+                {
+                    fine = true;
+                    return;
+                }
+            }
+            Thread.Sleep(100);  
+        }
+    }
+
+    static void Consumatore()
+    {
+        while (true)
+        {
+            int numero;
+            lock (lockCoda)
+            {
+                while (numeri.Count == 0)
+                {
+                    Monitor.Wait(lockCoda);
+                }
+                numero = numeri.Dequeue();
+            }
+            Console.WriteLine($"Consumato: {numero}");
+            Thread.Sleep(1000);
+            if (numero == 0)
+            {
+                return;
+            }
+        }
+    }
+}
